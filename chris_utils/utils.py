e3b0d52673fd1d74ef0779c84c6610aa7a3d6cfc
hcollingwood <hannah.collingwood@telespazio.com>
2025-09-02 16:14:46 +0100

    Check files are valid, show warning if not

--- chris_utils/utils.py
+++ chris_utils/utils.py
@@ -1,11 +1,13 @@
+import logging
 import os
 
 def get_list_of_files(inputs):
     files = []
+    error_message = "%s not recognised. Ensure that path is valid"
     for i in inputs:
         if os.path.isfile(i):
             files.append(i)
-        if os.path.isdir(i):
+        elif os.path.isdir(i):
             if i.endswith('.zarr'):
                 files.append(i)
             elif i.endswith('.SAFE'):
@@ -13,5 +15,8 @@ def get_list_of_files(inputs):
             else:
                 for file in os.listdir(i):
                     files.append(f"{i}/{file}")
+
+        else:
+            logging.error(error_message, i)
 
     return files
